d70a810b1a4c6457cf5bf16910cf6230be1507ab
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-12-08 13:39:57 +0530

    Adding the same button without using Xaml.

--- 0001_module_Introduction_to_WPF/WpfButtonSample/WpfButtonSample/MainWindow.xaml.cs
+++ 0001_module_Introduction_to_WPF/WpfButtonSample/WpfButtonSample/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfButtonSample
 {
@@ -23,6 +12,15 @@ namespace WpfButtonSample
         public MainWindow()
         {
             InitializeComponent();
+
+            Button MyButton = new Button();
+            MyButton.HorizontalAlignment = HorizontalAlignment.Center;
+            MyButton.VerticalAlignment = VerticalAlignment.Center;
+            MyButton.FontSize = 24;
+            MyButton.FontFamily = new System.Windows.Media.FontFamily("Candara");
+            MyButton.Foreground = Brushes.DarkRed;
+            MyButton.Content = "_Click me!";
+            MainGrid.Children.Add(MyButton);
         }
     }
 }
