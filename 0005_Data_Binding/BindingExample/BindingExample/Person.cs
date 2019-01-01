a9caa8fbd3960712ee2c31dc07bd6ddb6bcec67e
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-01-01 16:25:55 +0530

    Basic infrastructure before binding.

---
+++ 0005_Data_Binding/BindingExample/BindingExample/Person.cs
@@ -0,0 +1,20 @@
+﻿
+namespace BindingExample
+{
+    public class Person
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        private double _age;
+        public double Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+    }
+}
