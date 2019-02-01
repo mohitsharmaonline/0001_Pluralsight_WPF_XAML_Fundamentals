ea9768f52d60515a13f3f2e08786d2b311572871
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-02-01 20:06:55 +0530

    Initial code to start understanding of ObjectDataprovider

---
+++ 0005_Data_Binding/DataProvidersBindingExample/BindingExample/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace BindingExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // instance of object that will serve as datasource
+        Person src = new Person { Name = "Ian", Age = 34.6 };
+        List<Person> people = new List<Person>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            people.Add(src);
+            people.Add(new Person { Name = "Mike", Age = 62.5 });
+            people.Add(new Person { Name = "Brian", Age = 3.5 });
+
+            // in constrictor put datasource inside the datacontext of the window.
+            this.DataContext = people;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string message = src.Name + " is " + src.Age;
+            MessageBox.Show(message);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            src.Age += 0.1;
+
+            //AgeTextbox.GetBindingExpression(dp: TextBox.TextProperty).UpdateTarget();
+        }
+    }
+}
---
+++ 0005_Data_Binding/DataProvidersBindingExample/BindingExample/Person.cs
@@ -0,0 +1,40 @@
+﻿
+using System.ComponentModel;
+
+namespace BindingExample
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        private double _age;
+
+        public double Age
+        {
+            get => _age;
+            set
+            {
+                if(value != _age)
+                {
+                    _age = value;
+                    OnPropertyChanged(nameof(Age));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
