b17d08114d9ab96439b8454cdaf58af5850e1a12
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-02-13 21:58:53 +0530

    End of Converters

---
+++ 0005_Data_Binding/BindingExampleForWritingDataSource/BindingExample/AgeCheckConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BindingExample
+{
+    public class AgeCheckConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double age = System.Convert.ToDouble(value);
+            return age <= 21;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
