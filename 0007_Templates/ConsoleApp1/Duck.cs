cd4189fb841390ea61947ef4e5504ac18fc7b883
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-03-31 10:00:22 +0530

    Intro to Templates module expanded.

---
+++ 0007_Templates/ConsoleApp1/Duck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public abstract class Duck
+    {
+        public void Quack()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Swim()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public abstract void Display();        
+    }
+}
